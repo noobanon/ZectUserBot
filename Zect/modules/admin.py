1a9ae8b906f88253ce230219610e97a93fc9c219
•/ ＭＲ ∆ＮOＮ <51384033+noobanon@users.noreply.github.com>
2021-02-23 20:24:48 +0530

    Update admin.py

--- Zect/modules/admin.py
+++ Zect/modules/admin.py
@@ -29,6 +29,69 @@ CMD_HELP.update(
     }
 )
 
+@app.on_message(filters.command("promote", PREFIX) & filters.me)
+async def promote(_, message: Message):
+    if message.chat.type in ['group', 'supergroup']:
+        cmd = message.command
+        can_promo = await admin_check(message)
+        if can_promo:
+            try:
+                if message.reply_to_message:
+                    user_id = message.reply_to_message.from_user.id
+                    custom_rank = get_emoji_regexp().sub('', ' '.join(cmd[1:]))
+                else:
+                    usr = await client.get_users(cmd[1])
+                    custom_rank = get_emoji_regexp().sub('', ' '.join(cmd[2:]))
+                    user_id = usr.id
+            except IndexError:
+                await message.delete()
+                return
+
+            if user_id:
+                try:
+                    await client.promote_chat_member(
+                        message.chat.id,
+                        user_id,
+                        can_change_info=True,
+                        can_delete_messages=True,
+                        can_restrict_members=True,
+                        can_invite_users=True,
+                        can_pin_messages=True,
+                    )
+
+                    await asyncio.sleep(2)
+                    await client.set_administrator_title(
+                        message.chat.id, user_id, custom_rank,
+                    )
+                    await message.delete()
+                except UsernameInvalid:
+                    await edit_or_reply("None User Found")
+                    await asyncio.sleep(5)
+                    await message.delete()
+                    return
+                except PeerIdInvalid:
+                    await edit_or_reply("invalid User Or Chat")
+                    await asyncio.sleep(5)
+                    await message.delete()
+                    return
+                except UserIdInvalid:
+                    await edit_or_reply("User id is invalid")
+                    await asyncio.sleep(5)
+                    await message.delete()
+                    return
+
+                except ChatAdminRequired:
+                    await edit_or_reply("You don't have permission to do this")
+                    await asyncio.sleep(5)
+                    await message.delete()
+                    return
+
+        else:
+            await edit_or_reply("You don't have permission to do this")
+            await asyncio.sleep(5)
+            await message.delete()
+    else:
+        await message.delete()
 
 @app.on_message(filters.command("ban", PREFIX) & filters.me)
 async def ban_hammer(_, message: Message):
